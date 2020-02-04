5172c90b21e54bdcedb83486b76b441b2c81b163
kaverndsp <kaverndsp@users.noreply.github.com>
2020-02-04 14:52:00 -0600

    dashboard working. user link on dashboard is placeholder for the users profile image but links to a simple user settings form

--- src/loggedin/dashboard.js
+++ src/loggedin/dashboard.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {logo} from "../../src/sixrNav.png"
+import logo from "../sixrNav.png"
 import { Link, Route, Switch } from "react-router-dom";
 import Project from "../components/navigation/project";
 import Campaign from "../loggedin/campaign";
+import Settings from "../loggedin/settings";
 
 export const Dashboard = () => {
-  return (
+  
+  
+    return (
     <>
     <div className="header">
       <div className="nav-logo">
@@ -16,12 +19,13 @@ export const Dashboard = () => {
       <nav className="nav-links">
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/project">Create Campaign</Link>
-        <Link to="/"></Link>
+        <Link to="/settings">User</Link>
       </nav>
     </div>
       <Switch>
         <Route exact path="/dashboard" component={Campaign} />
         <Route path="/project" component={Project} />
+        <Route path="/settings" component={Settings} />
     </Switch>
     </>
   );
---
+++ src/loggedin/settings.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SettingsForm from "../forms/settingsform";
+
+
+
+
+const Settings = () => {
+
+
+    return(
+        <div>
+            <h1>Your Settings</h1>
+            <SettingsForm/>
+
+        </div>
+    )
+}
+
+
+
+export default Settings;
